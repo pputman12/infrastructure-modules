3bc93b5cd55502ce15edb5a241c16065a8bc2ff7
Parick Putman <patrick@teramind.co>
2022-01-02 02:42:15 -0600

    added okta-users variables

--- okta-users/variables.tf
+++ okta-users/variables.tf
@@ -6,3 +6,34 @@
 variable "okta_users" {
   type = any
 }
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+# HASHICORP VAULT VARIABLES 
+# The vault server IP and Port, along with the path to our okta api token stored securely in vault
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+variable "vault_address" {
+  description = "Hashicorp Vault Server Address"
+  type        = string
+}
+
+variable "vault_okta_secret_path" {
+  description = "The path to access the okta credentials in Vault"
+  type        = string
+}
+
+
+variable "okta_org_name" {
+  description = "The okta account to connect to"
+  type        = string
+}
+
+
+variable "okta_account_url" {
+  description = "base okta url"
+  type        = string
+}
+
+variable "okta_api_token" {
+  type    = string
+}
