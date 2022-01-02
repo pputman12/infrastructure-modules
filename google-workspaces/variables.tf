b9d04bbe24a9a769a6b8bf3c8bea00d67d3e9324
Parick Putman <patrick@teramind.co>
2022-01-02 01:16:41 -0600

    finished setting up workspaces for terragrunt

--- google-workspaces/variables.tf
+++ google-workspaces/variables.tf
@@ -32,7 +32,10 @@ variable "okta_api_token" {
   type    = string
 }
 
-
+#-------------------------------------------------------------------------------------------------------------------------------------
+# APPLICATION CONFIGURATION
+# Configuration for the saml application
+#-------------------------------------------------------------------------------------------------------------------------------------
 
 
 variable "app_name" {
@@ -58,43 +61,3 @@ variable "app_settings_json" {
   type    = map(any)
   default = {}
 }
-
-
-
-
-
-#-------------------------------------------------------------------------------------------------------------------------------------
-# HASHICORP VAULT VARIABLES 
-# The vault server IP and Port, along with the path to our okta api token stored securely in vault
-#-------------------------------------------------------------------------------------------------------------------------------------
-
-variable "vault_address" {
-  description = "Hashicorp Vault Server Address"
-  type        = string
-}
-
-variable "vault_okta_secret_path" {
-  description = "The path to access the okta credentials in Vault"
-  type        = string
-}
-
-#-------------------------------------------------------------------------------------------------------------------------------------
-# OKTA API CREDENTIALS
-# Credentials for the okta api
-#-------------------------------------------------------------------------------------------------------------------------------------
-
-variable "okta_org_name" {
-  description = "The okta account to connect to"
-  type        = string
-}
-
-variable "okta_account_url" {
-  description = "base okta url"
-  type        = string
-}
-
-variable "okta_api_token" {
-  type    = string
-}
-
-
