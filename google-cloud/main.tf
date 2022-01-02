e9febd589c2e74c3ec99c2e75efbbb85cde9b24c
Parick Putman <patrick@teramind.co>
2022-01-02 02:09:50 -0600

    moved duplicate provider

--- google-cloud/main.tf
+++ google-cloud/main.tf
@@ -17,14 +17,6 @@ terraform {
   }
 }
 
-
-provider "google" {
-  project     = var.google_terraform_project
-  region      = var.google_region
-  zone        = var.google_zone
-  credentials = file(var.google_credentials)
-}
-
 provider "okta" {
   org_name  = var.okta_org_name
   base_url  = var.okta_account_url
@@ -52,11 +44,11 @@ data "vault_generic_secret" "okta_creds" {
 #-------------------------------------------------------------------------------------------------------------------------------------
 
 
-provider "googleworkspace" {
-  customer_id             = var.google_customer_id
-  impersonated_user_email = var.google_impersonated_user_email
-  credentials             = file(var.google_credentials)
-  oauth_scopes            = var.google_oauth_scopes 
+provider "google" {
+  project     = var.google_terraform_project
+  region      = var.google_region
+  zone        = var.google_zone
+  credentials = file(var.google_credentials)
 }
 
 
