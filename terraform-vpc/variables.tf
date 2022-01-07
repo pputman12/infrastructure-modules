79b6a3a5d5a6156b2f209d6310d076ab0a31f4e1
Parick Putman <patrick@teramind.co>
2022-01-07 14:50:32 -0600

    commenting out vpc configuration

--- terraform-vpc/variables.tf
+++ terraform-vpc/variables.tf
@@ -4,17 +4,16 @@ variable "aws_region" {
 
 variable "vpc_cidr_block" {
   type    = string
-  default = "10.0.0.0/16"
+#  default = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
-  default = "10.0.1.0/24"
+#  default = "10.0.1.0/24"
   type    = string
 }
 
 variable "incoming_cidr_blocks_ssh" {
   type    = list(string)
-#  default = ["68.190.0.0/16"]
 }
 
 variable "aws_subnet1_az" {
