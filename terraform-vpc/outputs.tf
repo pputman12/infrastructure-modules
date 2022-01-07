a92a7486cb1ec8c97fc9cf4f4f223a6efc3dcb26
Parick Putman <patrick@teramind.co>
2022-01-06 20:09:33 -0600

    added terraform vpc module

---
+++ terraform-vpc/outputs.tf
@@ -0,0 +1,11 @@
+output "external_vpc" {
+  value = aws_vpc.prod_vpc
+}
+
+output "external_subnet" {
+  value = aws_subnet.prod_subnet_public_1
+}
+
+output "ssh_security_group" {
+   value = aws_security_group.ssh_allowed
+}
