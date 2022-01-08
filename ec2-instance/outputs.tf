dd1b53252b341857189995ac93274e36add7b580
Parick Putman <patrick@teramind.co>
2022-01-08 04:08:20 -0600

    organized modules

--- ec2-instance/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2_instance_ip" {
-  value = aws_eip.ec2_instance_ip.public_ip
-}
--- vault-prov/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "backend" {
-  value = vault_aws_secret_backend.aws.path
-}
-
-output "role" {
-  value = vault_aws_secret_backend_role.admin.name
-}
